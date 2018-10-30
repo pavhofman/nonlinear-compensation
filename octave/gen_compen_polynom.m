57889aad8a30d7de23a9e9bd32121a8a7430da61
Pavel Hofman <pavel.hofman@ivitera.com>
2018-10-30 11:16:12 +0100

    Initial version of Octave scripts

---
+++ octave/gen_compen_polynom.m
@@ -0,0 +1,132 @@
+#pkg load miscellaneous
+#pkg load control
+#pkg load signal
+#pkg load optim
+
+
+# values obtained from measure_phase.m
+# TODO - merging the two scripts. 
+# But polynomial can be generated only when the phase shift is correctly detected 
+# (i.e. only one peak, recorded and reference waveforms are at the end properly aligned!
+
+# all values should be identical for both channels
+offset = 9600;
+phaseshift = -0.366886759760545;
+measfreq = 1000;
+
+# at least 2 secs
+wavPath = "/home/hestia/tmp/octave/wavs/juli/recorded.wav";
+
+# 1 = right, 2 = left
+channel = 1;
+
+# calculating FFT + first 10 harmonics
+# return: peaks [ freq , power, angle ]
+function [peaks, x, y] = getHarmonics(samples, fs)
+  # fft length (must be 2^n)
+  nfft = 2^16;
+  
+  data = samples(1:nfft);
+  winlen = length(data);
+  winfun = hanning(winlen);
+  waudio = zeros(nfft, 1);
+  waudio(1:winlen) = data .* winfun;
+  yf = fft(waudio);
+  nffto2 = nfft / 2;
+  # fft normalization and window compensation
+  y = abs(yf(1:nffto2)) / (nffto2 * mean(winfun));
+  # logarithmic y-axis
+  y = 20 * log10(y);
+  x = linspace(1, fs/2, nffto2);
+
+  # finding peaks
+  # [ freq , power, angle ]
+  merged = [x', y, arg(yf(1:nffto2)) * 180/pi];
+  sorted = sortrows(merged, [-2]);
+
+  peaks = repmat([0,-999,0],10,1);
+  ff = sorted(1, 1);
+  fa = sorted(1, 2);
+  fp = sorted(1, 3);
+  peaks(1, :) = [ ff, fa, 0 ];
+  binwidth = fs / nfft;
+  skip=int32(1.5*ff/binwidth);
+  merged2 = merged(skip:nffto2, :);
+  sorted2 = sortrows(merged2, [-2]);
+  for i = 1:100
+      r = sorted2(i, :);
+      n =int32(r(1) / ff);
+      if abs(r(1) - (n * ff)) < 10
+          if n <= 10 && peaks(n, 1) == 0
+              peaks(n, :) = [r(1), r(2), mod(r(3) - fp, 360)];
+          end
+      end
+  end
+endfunction
+
+function drawHarmonics(x, y, label, plotID, plotsCnt)
+  subplot(plotsCnt,1,plotID);
+  semilogx(x, y, 'linewidth', 1.5, 'color', 'black');
+  grid('on');
+  ylim([-180 0])
+  axis([900 10000]);
+  xlabel('Frequency (Hz)', 'fontsize', 10);
+  ylabel('Magnitude (dB)', 'fontsize', 10);
+  title(label);
+  % change the tick labels of the graph from scientific notation to floating point:
+  xt = get(gca,'XTick');
+  set(gca,'XTickLabel', sprintf('%.0f|',xt))
+endfunction
+
+function showFFT(series, label, plotID, fs, plotsCnt)
+  [ peaks, x, y ] = getHarmonics(series, fs);
+  drawHarmonics(x, y, label, plotID, plotsCnt);
+
+  fprintf([label ':\n']);
+  fprintf('%8.2f Hz, %7.2f dB, %7.2f dg\n', peaks');
+endfunction
+
+
+format long e;
+[recorded, fs] = audioread(wavPath);
+if columns(recorded) > 1
+    % convert to mono
+    recorded = recorded(offset + 1:end, channel);
+end
+
+t = 0:1/fs:length(recorded)/fs;
+t = t(1:length(recorded));
+
+refGain = db2mag(-2);
+reference = cos(2*pi * measfreq * t + phaseshift) * refGain;
+reference = reference';
+
+polyCoeff = polyfit(recorded, reference, 5);
+plotsCnt = 2;
+showFFT(recorded, "Recorded", 1, fs, plotsCnt);
+
+
+recovered = polyval(polyCoeff, recorded);
+showFFT(recovered, "Recovered", 2, fs, plotsCnt);
+
+printf("Compensation Polynomial (copy to capture route 'polynom [ xx xx xx ...]'):\n");
+disp(fliplr(polyCoeff));
+return;
+
+
+#  arx identification
+xin = reference(1:100000);
+yout = recorded(1:100000);
+# TODO - this is incorrect, we need the other direction
+z=iddata(yout, xin, 1/48000);
+[sys, x0] = arx(z,"na", 5, "nb", 1);
+
+
+[yA,tA,xA] = lsim(sys,xin);
+plotOffset=1000;
+plot(yout(plotOffset+1:plotOffset+50),'k;measured output;',yA(plotOffset+1:plotOffset+50),'r;simulated output;')
+
+sys
+showFFT(yA(1000:end), "Model", 2, fs, plotsCnt);
+
+return;
---
+++ octave/measure_phase.m
@@ -0,0 +1,93 @@
+
+# at least 2 secs of recording, stereo or mono
+wavPath = "/home/hestia/tmp/octave/wavs/juli/recorded.wav";
+# 1 = right, 2 = left
+channel = 2;
+
+
+# measured frequency (TODO - detect/adjust automatically by measuring relative phaseshift of reference and recorded at the end of recorded/reference)
+measfreq = 1000.000;
+
+format long;
+[recorded, fs] = audioread(wavPath);
+
+if columns(recorded) > 1
+%    % convert to mono
+    recorded = recorded(:, channel);
+end
+
+# Offset must be large enough to skip samples from the first alsa period where some garbled data appears.
+# Alsa period size could be read precisely from /proc/asound/cardXXX/pcmXc/sub0/hw_params
+# Safe bet is 200ms.
+offset = 0.2 * fs;
+
+# For the phase detection to work precisely, fft must be applied to number of samples corresponding exctly to whole measfreq periods (to the sample)
+# Too many periods can result in imprecise phase detection due to instable fs lock. Just a few periods actually suffice.
+
+# Warning - 44100Hz FS requires multiples of 10 for measfreq = 1kHz (10 * 44100/1000  => integer )
+
+# number of measfreq periods for phase detection
+periods = 10;
+
+recorded = recorded(offset + 1:end);
+
+samplesInPeriods = periods * fs/measfreq;
+x = recorded(1:offset + samplesInPeriods);
+
+ys = fft(x);
+ys = fftshift(ys);
+# remove frequency mirror
+bins = length(x)/2;
+ys = ys(bins + 1:length(x));
+
+f = linspace(1, fs/2, bins);
+
+
+# We want to see ys maximum. We do it by zeroing all ys under certain level (note - we did not normalize, raw numbers!)
+# TODO - this procedure needs more robustness
+yslimit = 10;
+ys(abs(ys) < yslimit) = 0;
+
+# plotting abs(fft)
+subplot(3,1,1);
+stem(f,abs(ys));
+xlabel 'Frequency (Hz)';
+ylabel '|y|';
+grid;
+
+phs = angle(ys);
+
+# plotting phase(fft)
+subplot(3,1,2);
+stem(f,phs/pi)
+xlabel 'Frequency (Hz)';
+ylabel 'Phase / \pi';
+grid;
+
+
+# We need to find phase of the largest fft value
+[max_fft, index] =max(ys);
+phaseshift = phs(index);
+
+offset
+phaseshift
+
+# plotting phase alignment of calculated reference sine and recorded at the end of the array
+
+# generating the reference sine
+t = 0:1/fs:length(recorded)/fs;
+t = t(1:length(recorded));
+refGain = db2mag(-3);
+reference = cos(2*pi * measfreq * t + phaseshift)* refGain;
+
+# finding end of arrays
+samplesPlotted = 100;
+# just in case the final samples of the wav are garbled
+offsetFromEnd = 100;
+endPos = length(recorded) - offsetFromEnd;
+lowT = endPos - samplesPlotted;
+highT = endPos;
+
+# the curves must be exactly phase-aligned!!!
+subplot(3,1,3);
+plot((lowT:highT), recorded(lowT:highT), "-", (lowT:highT), reference(lowT:highT), "*");
