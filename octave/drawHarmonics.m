bbf7d55c6a7af3faf9cf2634cade98484f322a1d
Pavel Hofman <pavel.hofman@ivitera.com>
2018-11-03 17:42:51 +0100

    OCTAVE: non-zero values for semilogx() in drawHarmonics

--- octave/drawHarmonics.m
+++ octave/drawHarmonics.m
@@ -1,5 +1,6 @@
 function drawHarmonics(x, y, label, plotID, plotsCnt, use_ylim = [-140 0])
   subplot(plotsCnt,1,plotID);
+  x(x == 0) = 10^-10;
   semilogx(x, y, 'linewidth', 1.5, 'color', 'black');
   grid('on');
   ylim(use_ylim)
