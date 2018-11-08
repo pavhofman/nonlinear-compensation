3ac867b9113fe68251a512442f4dcede604729f9
Miroslav Rudisin <miero@seznam.cz>
2018-11-08 19:51:44 +0100

    getHarmonics: fixed phase computation & speed increase

--- octave/getHarmonics.m
+++ octave/getHarmonics.m
@@ -1,59 +1,57 @@
-% calculating FFT + first 10 harmonics
-% return: peaks [ freq , power, angle ]
-function [peaks, x, y] = getHarmonics(samples, fs)
-  % fft length - even number
-  nfft = min(floor(length(samples)/2) * 2, 2^17);
-
-  % determine frequency with max. amplitude
+% calculating FFT + harmonics up to Fs
+%
+% computes FFT from number of samples that is maximum whole multiple of Fs
+%
+% hanning window is used if precise_amplitude is 0
+% flattop window is used if precise_amplitude is 1
+%
+% frequency in peaks is non-zero only if the harmonics is a peak
+% if there is no peak peaks.amplitude is maximum of 3 bins which are
+% the most close to the expected frequency of the harmonics
+%
+% returns:
+%   peaks [ frequency , amplitude_in_dB, angle_in_degrees ]
+%   x - freqencies
+%   y - amplitudes_in_dB
+%
+function [peaks, x, y] = getHarmonics(samples, Fs, precise_amplitude = 0)
+  nfft = Fs * floor(length(samples)/Fs);
   data = samples(1:nfft);
-  winfun = flattopwin(length(data));
-  yf = fft(data .* winfun);
-  nffto2 = nfft / 2;
-  [ymax, iymax] = max(abs(yf(1:nffto2)));
-  maxfreq = (iymax-1)*fs/2/(nffto2-1);
-
-  % determine number of bins required for exact phase measurememnt
-  nsamplesInPeriod = uint32(fs/maxfreq);
-  nperiods = length(samples) / nsamplesInPeriod;
-  if (nperiods > 10)
-      nperiods = 10;
+  if precise_amplitude == 0
+      winfun = flattopwin(length(data));
+  else
+      winfun = hanning(length(data));
   end
-  nfft = nperiods * nsamplesInPeriod;
-  nfft -= mod(nfft, 2);
+  waudio = data .* winfun;
+  yf = fft(waudio);
+  binwidth = Fs / nfft;
+  nffto2 = (nfft / 2) + 1;
 
-  % compute fft again with rectangle window
-  data = samples(1:nfft);
-  yf = fft(data);
-  nffto2 = nfft / 2;
-  % fft normalization and window compensation
-  y = abs(yf(1:nffto2)) / double(nffto2);
-  % logarithmic y-axis
-  y = 20 * log10(y);
-  y(y < -145) = -999;
-  x = fs/2 * linspace(0, 1, nffto2)';
-
-  % finding peaks
-  % [ freq , power, angle ]
-  merged = [x, y, arg(yf(1:nffto2)) * 180/pi];
-  sorted = sortrows(merged, [-2]);
-  peaks = repmat([0,-999,0],10,1);
-  ff = sorted(1, 1);
-  fa = sorted(1, 2);
-  fp = sorted(1, 3);
-  maxfreq = int32(maxfreq);
-  peaks(1, :) = [ maxfreq, fa, 0 ];
-  % Notice: retype is required to force double precision of the output
-  binwidth = fs / double(nffto2);
-  skip=int32(1.5*ff/binwidth);
-  merged2 = merged(skip:nffto2, :);
-  sorted2 = sortrows(merged2, [-2]);
-  for i = 1:length(sorted2)
-      r = sorted2(i, :);
-      n = int32(r(1) / ff);
-      if abs(r(1) - (n * ff)) < binwidth
-          if n <= 10 && peaks(n,  1) == 0
-              peaks(n, :) = [n * maxfreq, r(2), mod(r(3) - fp, 360)];
+  x = double(Fs/2) * linspace(0, 1, nffto2);
+  y = 20 * log10(abs(yf(1:nffto2)) / (nffto2 * mean(winfun)));
+  [ymax, iymax] = max(y);
+  fundfreq = (iymax - 1) * binwidth;
+  peaks = [];
+  for nh = 1:floor(Fs/2/fundfreq)
+      i = (nh * (iymax - 1)) + 1;
+      hp = arg(yf(i)) * 180/pi;
+      if (nh > 1)
+          hp = mod(hp - fp, 360);
+      else
+          fp = hp;
+          hp = 0;
+      end
+      if (i + 1 < nffto2) && y(i) > -130 && (y(i - 1) < (y(i))) && ((y(i)) > y(i + 1))
+          peaks(nh, :) = [x(i), y(i), hp];
+      else
+          % do not report frequency if it is not a peak
+          if (i + 1 < nffto2)
+              nhy = [y(i-1),y(i),y(i+1)];
+          else
+              % skip y(nffto2) as it requires special weighting
+              nhy = [y(i-1)];
           end
+          peaks(nh, :) = [0, max(nhy), hp];
       end
   end
 endfunction
