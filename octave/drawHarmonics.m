779e7c4693f54a2204a1fb23fdd43a88edc252bf
Miroslav Rudisin <miero@seznam.cz>
2018-11-11 13:49:40 +0100

    hide freqs under 10Hz and custom xlim

--- octave/drawHarmonics.m
+++ octave/drawHarmonics.m
@@ -1,12 +1,17 @@
-function drawHarmonics(x, y, label, plotID, plotsCnt, use_ylim = [-150 0])
+function drawHarmonics(x, y, label, plotID, plotsCnt, use_ylim = [-150 0], use_xlim = [900 10000])
   y = 20 * log10(y);
   subplot(plotsCnt,1,plotID);
+  % skip frequencies under 10Hz
+  skip = 1;
+  while x(skip) < 10
+      skip++;
+  endwhile
   % semilogx requires values > 0
   x(x == 0) = 10^-10;
-  semilogx(x, y, 'linewidth', 1.5, 'color', 'black');
+  semilogx(x(skip:end), y(skip:end), 'linewidth', 1.5, 'color', 'black');
   grid('on');
   ylim(use_ylim)
-  axis([900 10000]);
+  xlim(use_xlim);
   xlabel('Frequency (Hz)', 'fontsize', 10);
   ylabel('Magnitude (dB)', 'fontsize', 10);
   title(label);
