dc363e6f68a09b481be8dc2efe76855708dc2acb
Pavel Hofman <pavel.hofman@ivitera.com>
2018-11-23 16:55:33 +0100

    Renaming vars in genCompenReference.m to their specific meanings. Frequency/phase speed calculated by peak frequency ratio instead of its position in distortPeaks list"

--- octave/genCompenReference.m
+++ octave/genCompenReference.m
@@ -1,27 +1,33 @@
 % Generates compensation reference (distortion peaks with inverted phase). Supports only single fundamental frequency, for now.
-function reference = genCompenReference(fundPeaks, distortPeaks, measuredPeaks, fs, startingT, samplesCnt)
+function compenSignal = genCompenReference(fundPeaks, distortPeaks, measuredPeaks, fs, startingT, samplesCnt)
   % first harmonics = zero
-  reference = zeros(samplesCnt, 1);
+  compenSignal = zeros(samplesCnt, 1);
   % for now only single fundamental frequency
-  measfreq = measuredPeaks(1, 1);
-  origAmpl = fundPeaks(1, 2);
-  origPhase = fundPeaks(1, 3);
+  measFreq = measuredPeaks(1, 1);
+  origFundAmpl = fundPeaks(1, 2);
+  origFundPhase = fundPeaks(1, 3);
   
-  currAmpl = measuredPeaks(1, 2);
-  currPhase = measuredPeaks(1, 3);
+  currFundAmpl = measuredPeaks(1, 2);
+  currFundPhase = measuredPeaks(1, 3);
   
-  compenPhase = currPhase - origPhase;
-  scale = currAmpl / origAmpl;
+  currFundPhaseShift = currFundPhase - origFundPhase;
+  scale = currFundAmpl / origFundAmpl;
   step = 1/fs;
-  t = linspace(startingT, startingT + (samplesCnt - 1) * step, samplesCnt)'; 
+  t = linspace(startingT, startingT + (samplesCnt - 1) * step, samplesCnt)';
+
   for i = (1:length(distortPeaks))
-    gain = distortPeaks(i, 2);
-    if (gain > 1e-8)
-      % inverted phase 
-      % harmonicNb - for now works only with single fundamental frequency - i.e. first row = second harmonics
-      harmonicNb = i + 1;
-      shift = (distortPeaks(i, 3) + compenPhase * harmonicNb) - pi;
-      reference += cos(2*pi * harmonicNb * measfreq * t + shift) * gain * scale;
+    origDistortGain = distortPeaks(i, 2);
+    if (origDistortGain > 1e-8)
+      distortFreq = distortPeaks(i, 1);
+      distortFreqRatio = distortFreq/measFreq;
+      origDistortPhase = distortPeaks(i, 3);
+      % current phase distortion
+      currentDistortPhase = origDistortPhase + currFundPhaseShift * distortFreqRatio;
+      % inverted phase
+      compenDistortPhase = currentDistortPhase - pi;
+      compenDistortGain = origDistortGain * scale;
+      % compensation signal for this distortion
+      compenSignal += cos(2*pi * distortFreq * t + compenDistortPhase) * compenDistortGain;
      endif
   endfor
 endfunction
