fc0bc57b573d36dfe8a799ba8532e4dd8e7b3238
Miroslav Rudisin <miero@seznam.cz>
2018-11-24 19:15:24 +0100

    find also intermodulation frequencies between two strongest harmonics

--- octave/findHarmonicsFromFFT.m
+++ octave/findHarmonicsFromFFT.m
@@ -36,6 +36,7 @@ function [fundPeaks, distortPeaks, errorMsg] = findHarmonicsFromFFT(Fs, nfft, x,
       return
   end
 
+  fundPeakBins = [];
   for b = find(y(skip_bins:end) >= ymax/10)'
       bb = skip_bins + b - 1;
       if ((bb < nffto2) && (y(bb) < y(bb+1))) || (y(bb-1) > y(bb))
@@ -45,6 +46,7 @@ function [fundPeaks, distortPeaks, errorMsg] = findHarmonicsFromFFT(Fs, nfft, x,
           errorMsg = 'too many fundamental peaks';
           break
       end
+      fundPeakBins = [fundPeakBins; bb];
       fundPeaks = [fundPeaks; x(bb), y(bb), arg(yc(bb))];
       lasth = min(20, floor(Fs / 2 / x(bb)));
       for nh = 1:lasth
@@ -53,6 +55,24 @@ function [fundPeaks, distortPeaks, errorMsg] = findHarmonicsFromFFT(Fs, nfft, x,
               continue
           end
           distortPeaks = [distortPeaks; x(i), y(i), arg(yc(i))];
+      end
+  end
+
+  if rows(fundPeaks) == 2
+      % compute intermodulations between two strongest frequencies (5th order max)
+      for imd_order = 2:5
+          for o1 = 1 : (imd_order-1)
+              o2 = imd_order - o1;
+              i1 = o1 * (fundPeakBins(1) - 1);
+              i2 = o2 * (fundPeakBins(2) - 1);
+              for i = [ i1 + i2 + 1, i1 - i2 + 1, i2 - i1 + 1 ];
+                  if (i < 1) || (i > nffto2) || (y(i) >= (ymax / 10))
+                      % ignore aliased frequencies and stronger than 1/10 of the strongest one
+                      continue
+                  end
+                  distortPeaks = [distortPeaks; x(i), y(i), arg(yc(i))];
+              end
+          end
       end
   end
 endfunction
