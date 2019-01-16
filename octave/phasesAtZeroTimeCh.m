5b8095cbd728d9a638dfcc23929d0672e8a5889b
Pavel Hofman <pavel.hofman@ivitera.com>
2019-01-16 19:14:45 +0100

    Calfiles are stored with phases at time zero (i.e. when all fund phases = 0). This is required for planned interpolation of distort peaks - all must be based on the same time

---
+++ octave/phasesAtZeroTimeCh.m
@@ -0,0 +1,17 @@
+% shift all distort phases to time zero (i.e. where all fund phases = 0)
+function distortPeaksCh = phasesAtZeroTimeCh(fundPeaksCh, distortPeaksCh)  
+  zeroPhaseFundPeaksCh = fundPeaksCh;
+  % zero phase, keep rest
+  zeroPhaseFundPeaksCh(:, 3) = 0;
+  
+  timeOffset = determineTimeOffset(zeroPhaseFundPeaksCh, fundPeaksCh);
+  
+  % all distortions must be rotated timeOffset ahead
+  for i = (1:rows(distortPeaksCh))
+    distortFreq = distortPeaksCh(i, 1);
+    distortPhase = distortPeaksCh(i, 3);
+    % zero-time phase distortion = distortion phase - phase accumulated in timeOffset
+    zeroTimeDistortPhase = distortPhase - 2 * pi * distortFreq * timeOffset;
+    distortPeaksCh(i, 3) = zeroTimeDistortPhase;
+  endfor
+endfunction
--- octave/saveCalFile.m
+++ octave/saveCalFile.m
@@ -1,10 +1,14 @@
 function result = saveCalFile(fundPeaksCh, distortPeaksCh, fs, channelID, timestamp, deviceName, extraCircuit = '')
+      % shift all peaks to zero time
+      distortPeaksCh = phasesAtZeroTimeCh(fundPeaksCh, distortPeaksCh);
+      fundPeaksCh(:, 3) = 0;
       % has fund freqs, storing
       calRec.timestamp = timestamp;
       calRec.fundPeaks = fundPeaksCh;
       calRec.distortPeaks = distortPeaksCh;
       disp(calRec);
       freqs =  getFreqs(fundPeaksCh);
+      
       calFile = genCalFilename(freqs, fs, channelID, deviceName, extraCircuit);
       save(calFile, 'calRec');
       printf('Saved calfile %s\n', calFile);
