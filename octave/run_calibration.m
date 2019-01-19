b44a9e2455c71bae3233c4702499b1bf4876b743
Pavel Hofman <pavel.hofman@ivitera.com>
2019-01-19 19:01:45 +0100

    CAL: Turning off calibration after finishing, passing if no previous status set.
    Reason: Calibration is usually performed in series of steps and switching to compensation automatically is not required.

--- octave/run_calibration.m
+++ octave/run_calibration.m
@@ -1,13 +1,9 @@
 result = calibrate(buffer, fs, jointDeviceName, calExtraCircuit, restartCal);
 restartCal = false;
 if (result == FINISHED_RESULT)
-  % finished
-  % request joint-device compensation for calExtraCircuit
-  if (length(calExtraCircuit) > 0)
-    cmd = {COMPENSATE; jointDeviceName; calExtraCircuit};
-  else
-    cmd = {COMPENSATE; jointDeviceName};
+  % turn off calibration
+  status = removeFromStatus(status, CALIBRATING);
+  if isempty(status)
+    cmd = {PASS};
   endif
-  % tell analysis to re-read the updated calibration file
-  restartAnalysis = true;
 endif
