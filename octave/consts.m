852e828439e73f6b76e33a070a27728090f9cb5a
Pavel Hofman <pavel.hofman@ivitera.com>
2019-02-01 20:51:56 +0100

    CONSTS: adding strings for each status, all global

--- octave/consts.m
+++ octave/consts.m
@@ -19,15 +19,28 @@ SPLIT2 = 'split2';
 NO_CMD = '';
 
 % bits for statuses
-PAUSED = 0;
-PASSING = 1;
-CALIBRATING = 2;
-ANALYSING = 3;
-COMPENSATING = 4;
-DISTORTING = 5;
-GENERATING = 6;
-MEASURING = 7;
-SPLITTING = 8;
+global PAUSED = 1;
+global PASSING = 2;
+global CALIBRATING = 3;
+global ANALYSING = 4;
+global COMPENSATING = 5;
+global DISTORTING = 6;
+global GENERATING = 7;
+global MEASURING = 8;
+global SPLITTING = 9;
+
+
+global STATUS_NAMES = cell();
+STATUS_NAMES{PAUSED} = 'Paused';
+STATUS_NAMES{PASSING} = 'Passing';
+STATUS_NAMES{CALIBRATING} = 'Calibrating';
+STATUS_NAMES{ANALYSING} = 'Analysing';
+STATUS_NAMES{COMPENSATING} = 'Compensating';
+STATUS_NAMES{DISTORTING} = 'Distorting';
+STATUS_NAMES{GENERATING} = 'Generating';
+STATUS_NAMES{MEASURING} = 'Measuring';
+STATUS_NAMES{SPLITTING} = 'Splitting';
+
 
 %directions
 DIR_REC = 1;
@@ -50,4 +63,4 @@ ZEROMQ_PORT_REC = 5555;
 ZEROMQ_PORT_PLAY = 5556;
 
 % fixed generator amplitude for now - used when generating sine (genSine) and calculating split calibration (run_split)
-genAmpl = db2mag(-3);
+global genAmpl = db2mag(-3);
