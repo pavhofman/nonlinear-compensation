fc7436da9afa137c631e042a5543c5dabe77bab4
Pavel Hofman <pavel.hofman@ivitera.com>
2019-02-01 20:53:36 +0100

    Each status contributes with additional info in buildInfo.m Implemented for status GENERATING.

--- octave/buildInfo.m
+++ octave/buildInfo.m
@@ -5,19 +5,36 @@ function info = buildInfo()
   global measuredPeaks;
   global fundPeaks;
   global distortPeaks;
-  global genAmpl;
-  global genFreq;
+
   global fs;
   global direction;
 
   info.id = NA;
   info.time = time();
+  
   info.status = status;
+  for id = 1: length(status)
+    statusItem = status(id);
+    info = addStatusDetails(statusItem, info);
+  endfor
+  
   info.measuredPeaks = measuredPeaks;
   info.fundPeaks = fundPeaks;
   info.distortPeaks = distortPeaks;
-  info.genAmpl = genAmpl;
-  info.genFreq = genFreq;
+
   info.fs = fs;
   info.direction = direction;
+endfunction
+
+
+% adding details for each status item, if any
+function info = addStatusDetails(status, info)
+  global GENERATING;
+  switch status
+    case GENERATING
+      global genAmpl;
+      info.genAmpl = genAmpl;
+      global genFreq;
+      info.genFreq = genFreq;
+  endswitch
 endfunction
