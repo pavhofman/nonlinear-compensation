2c27afba284039c99503c4fd952d096a6bf1b1a9
Pavel Hofman <pavel.hofman@ivitera.com>
2019-02-03 17:08:24 +0100

    CTRL: method writeCmd() generates command IDs and includes them in cmd.info files.

--- octave/ctrl/writeCmd.m
+++ octave/ctrl/writeCmd.m
@@ -1,8 +1,17 @@
 % atomic writing commands to cmd file
-function writeCmd(cmd, cmdFilename)
+function cmdID = writeCmd(cmd, cmdFilename)  
+  persistent intCmdID = 1;
+  global CMD_ID_PREFIX;
+  
+  % ID5
+  cmdID = [CMD_ID_PREFIX int2str(intCmdID)];
+  
   [tmpfid, tmpFilename] = mkstemp([ tempdir() 'XXXXXX' ], true);
-  fprintf(tmpfid,"%s", cmd);
+  fprintf(tmpfid,"%s %s", cmdID, cmd);
   fclose(tmpfid);
   movefile(tmpFilename, cmdFilename);
   unlink(tmpFilename);
+  
+  % incrementing integer part of command ID for next call
+  ++intCmdID;
 endfunction
