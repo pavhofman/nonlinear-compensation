654c17e1a3b2dffbd7f7baa3999d887c08cfed9c
Pavel Hofman <pavel.hofman@ivitera.com>
2019-02-10 23:20:00 +0100

    COMPEN: passing info message 'No calfile found' to UI

--- octave/run_compensation.m
+++ octave/run_compensation.m
@@ -1,6 +1,7 @@
 % compensation running
 % generating buffer-length of compensation reference
 result = RUNNING_OK_RESULT;
+msg = '';
 for channelID = 1:columns(buffer)
   measuredPeaksCh = measuredPeaks{channelID};
   distortPeaksCh = distortPeaks{channelID};
@@ -17,9 +18,14 @@ for channelID = 1:columns(buffer)
     result = FAILING_RESULT;
     % resetting calfile
     compenCalFiles{channelID} = '';
+    if hasAnyPeak(measuredPeaksCh) && ~hasAnyPeak(fundPeaksCh)
+      % incoming signal, yet not fundPeaks from calfile - no calfile found
+      msg = [msg ' ' 'No calib. for CH' num2str(channelID) '.'];
+    endif
   endif
 endfor
 
 setStatusResult(COMPENSATING, result);
+setStatusMsg(COMPENSATING, msg);
 % advancing startingT to next cycle
 startingT += rows(buffer) * 1/fs;
