dc3109bd812b973f1f21bcbf49ba2a3568b6ae3e
Pavel Hofman <pavel.hofman@ivitera.com>
2019-02-17 19:52:19 +0100

    DISTORT: limiting distortion output to <-1, 1>

--- octave/run_distortion.m
+++ octave/run_distortion.m
@@ -1,4 +1,7 @@
 % introduce distortion to buffer
 if ~isempty(distortPoly)
   buffer = polyval(distortPoly, buffer);
+  % clipping to <-1, 1>
+  buffer(buffer > 1) = 1;
+  buffer(buffer < -1) = -1;
 endif
