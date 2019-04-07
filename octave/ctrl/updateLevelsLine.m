7b6f8e92ad6dc27dce66c7eb063bb30928b24927
Pavel Hofman <pavel.hofman@ivitera.com>
2019-04-07 21:57:50 +0200

    UI: fixing comment

--- octave/ctrl/updateLevelsLine.m
+++ octave/ctrl/updateLevelsLine.m
@@ -3,7 +3,7 @@ function updateLevelsLine(levels, line, x)
   % plotting levels
   x = repmat(x, rows(levels), 1);
   if length(levels) == 2
-    % shiting second level to the right by CH_DISTANCE_X 
+    % shifting second level to the right by CH_DISTANCE_X 
     global CH_DISTANCE_X;
     x(2) += CH_DISTANCE_X;
   endif
