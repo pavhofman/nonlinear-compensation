c7ae91d19395092274410843b001794ba8c74e51
Pavel Hofman <pavel.hofman@ivitera.com>
2019-04-18 20:23:26 +0200

    UI: methods setEnabled()/setVisible() support array of items.

--- octave/ctrl/setEnabled.m
+++ octave/ctrl/setEnabled.m
@@ -1,10 +1,13 @@
-function setEnabled(item, enable)
-  isEnabled = strcmp(get(item, 'enable'), 'on');
-  if enable != isEnabled
-    if enable
-      set(item, 'enable', 'on');
-    else
-      set(item, 'enable', 'off');
+function setEnabled(items, enable)
+  for id = 1:length(items)
+    item = items(id);
+    isEnabled = strcmp(get(item, 'enable'), 'on');
+    if enable != isEnabled
+      if enable
+        set(item, 'enable', 'on');
+      else
+        set(item, 'enable', 'off');
+      endif
     endif
-  endif
+  endfor
 endfunction
--- octave/ctrl/setVisible.m
+++ octave/ctrl/setVisible.m
@@ -1,10 +1,13 @@
-function setVisible(item, visible)
-  isVisible = strcmp(get(item, 'visible'), 'on');
-  if visible != isVisible
-    if visible
-      set(item, 'visible', 'on');
-    else
-      set(item, 'visible', 'off');
+function setVisible(items, visible)
+  for id = 1:length(items)
+    item = items(id);
+    isVisible = strcmp(get(item, 'visible'), 'on');
+    if visible != isVisible
+      if visible
+        set(item, 'visible', 'on');
+      else
+        set(item, 'visible', 'off');
+      endif
     endif
-  endif
+  endfor
 endfunction
