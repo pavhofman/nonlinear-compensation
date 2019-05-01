e28716d3b609ad3a5a15cdc42ce4612841f683f9
Pavel Hofman <pavel.hofman@ivitera.com>
2019-05-01 18:26:17 +0200

    OCTAVE: disabling crash dumps in run_common.m

--- octave/run_common.m
+++ octave/run_common.m
@@ -1,6 +1,8 @@
 % script runs common code for mainRec/Play and mainCtrl. Defines global device names, creates required dirs, etc.
 % Requires loaded config.m files
 
+% no crash dumps
+crash_dumps_octave_core(false);
 
 if !exist(dataDir, 'dir')
   mkdir(dataDir);
