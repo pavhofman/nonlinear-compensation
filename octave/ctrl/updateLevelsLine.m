2ebe750ed414c8e92fc4505b674110bb4d38a0b8
Pavel Hofman <pavel.hofman@ivitera.com>
2019-09-01 17:53:34 +0200

    UI: transposing levels if not column in updateLevelsLine.m

--- octave/ctrl/updateLevelsLine.m
+++ octave/ctrl/updateLevelsLine.m
@@ -1,6 +1,10 @@
 % updating plot line with levels at x
 function updateLevelsLine(levels, line, x)  
   % plotting levels
+  % levels must be column
+  if isrow(levels)
+    levels = transpose(levels);
+  endif
   x = repmat(x, rows(levels), 1);
   if length(levels) == 2
     % shifting second level to the right by CH_DISTANCE_X 
