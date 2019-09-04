27974fc198efbf4923b99a6501e25a86a805f638
Pavel Hofman <pavel.hofman@ivitera.com>
2019-09-04 19:59:51 +0200

    COMPEN: all distortions are generated in matrix, removal of the loop in genCompenReference.m

--- octave/genCompenReference.m
+++ octave/genCompenReference.m
@@ -1,7 +1,8 @@
 % Generates compensation reference (distortion peaks with inverted phase). Supports single and dual frequencies
 function compenSignal = genCompenReference(fundLevelsCh, distortPeaksCh, measuredPeaksCh, fs, startingT, samplesCnt)
-  % first harmonics = zero
-  compenSignal = zeros(samplesCnt, 1);
+  % consts
+  persistent PI2 = 2 * pi;
+
   % for now only single fundamental frequency
   measFreq = measuredPeaksCh(1, 1);
   origFundAmpl = fundLevelsCh(1, 2);
@@ -14,18 +15,20 @@ function compenSignal = genCompenReference(fundLevelsCh, distortPeaksCh, measure
   
   scale = currFundAmpl / origFundAmpl;
   step = 1/fs;
-  t = linspace(startingT, startingT + (samplesCnt - 1) * step, samplesCnt)';
-  pi2 = 2 * pi;
-  for i = (1:rows(distortPeaksCh))
-    origDistortGain = distortPeaksCh(i, 2);
-    distortFreq = distortPeaksCh(i, 1);
-    origDistortPhase = distortPeaksCh(i, 3);
-    % current phase distortion = original distortion phase + additional phase accumulated in timeOffset
-    currentDistortPhase = origDistortPhase + pi2 * distortFreq * timeOffset;
-    % inverted phase
-    compenDistortPhase = currentDistortPhase - pi;
-    compenDistortGain = origDistortGain * scale;
-    % compensation signal for this distortion
-    compenSignal += cos(pi2 * distortFreq * t + compenDistortPhase) * compenDistortGain;     
-  endfor
+  t = linspace(startingT, startingT + (samplesCnt - 1) * step, samplesCnt);
+    
+  distortFreqs = distortPeaksCh(:, 1);
+  origDistortGains = distortPeaksCh(:, 2);
+  origDistortPhases = distortPeaksCh(:, 3);
+  % current phase distortion = original distortion phase + additional phase accumulated in timeOffset
+  currentDistortPhases = origDistortPhases + PI2 * distortFreqs * timeOffset;
+  % inverted phases
+  compenDistortPhases = currentDistortPhases - pi;
+  compenDistortGains = origDistortGains * scale;
+  % compensation signal for this distortion
+  compenSignals = cos(PI2 * distortFreqs * t + compenDistortPhases) .* compenDistortGains;
+  % adding all rows
+  compenSignal = sum(compenSignals, 1);
+  % output in rows
+  compenSignal = transpose(compenSignal);
 endfunction
