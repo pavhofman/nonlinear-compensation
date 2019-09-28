d355efec6758ace6a4d8fc5746e27635724fd412
Pavel Hofman <pavel.hofman@ivitera.com>
2019-09-28 16:41:09 +0200

    CALIB: moving function createCalFileDevSpecs.m to its only usage - genCalFilename.m

--- octave/genCalFilename.m
+++ octave/genCalFilename.m
@@ -36,6 +36,24 @@ function [filename, devSpecs] = genCalFilename(freqs, fs, compType, playChannelI
   filename = genDataPath(filename, dataDir);
 endfunction
 
+% devSpec: rows of cells {devName, chID}
+function devSpecs = createCalFileDevSpecs(compType, playChannelID, channelID)
+  global COMP_TYPE_JOINT;
+  global COMP_TYPE_PLAY_SIDE;
+  global COMP_TYPE_REC_SIDE;
+
+  global inputDeviceName;
+  global outputDeviceName;
+
+  switch compType
+    case COMP_TYPE_JOINT
+      devSpecs = {{outputDeviceName, playChannelID}; {inputDeviceName, channelID}};
+    case COMP_TYPE_PLAY_SIDE
+      devSpecs = {outputDeviceName, channelID};
+    case COMP_TYPE_REC_SIDE
+      devSpecs = {inputDeviceName, channelID};
+  endswitch
+endfunction
 
 
 %!test
