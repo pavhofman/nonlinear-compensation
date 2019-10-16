f1e60eae2c5c91ef0ced58bd30d3c0dfd8a6d1f8
Pavel Hofman <pavel.hofman@ivitera.com>
2019-10-16 18:35:53 +0200

    SCRATCH - removing directory octave-scratchbook

--- octave-scratchbook/getReferenceSignal.m
+++
@@ -1,8 +0,0 @@
-function [reference] = getReferenceSignal(recorded, fs, measfreq)
-    [refGain, phaseShift, ys, bins] = measurePhase(recorded, fs, measfreq, false);
-
-    % generating the reference sine
-    t = 0:1/fs:length(recorded)/fs;
-    t = t(1:length(recorded))';
-    reference = cos(2*pi * measfreq * t + phaseShift)* refGain;
-endfunction
