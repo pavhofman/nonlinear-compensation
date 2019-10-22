e1bc4707a8d5e3a8b0ef9ca190e377591b16f4ad
Pavel Hofman <pavel.hofman@ivitera.com>
2019-10-22 22:41:39 +0200

    ANALYSIS: minor change in fixMeasuredAmplPhase.m

--- octave/fixMeasuredAmplPhase.m
+++ octave/fixMeasuredAmplPhase.m
@@ -1,7 +1,8 @@
 % we need ampl > 0. If ampl < 0, we must invert phase
 function [ampl, phaseShift] = fixMeasuredAmplPhase(ampl, phaseShift)
   if (ampl < 0)
-    ampl = abs(ampl);
+    % inverting amplitude
+    ampl = -ampl;
     % inverting phase
     phaseShift += pi;
   endif
