c2290e970870134e0c1c20753f0d79185a62246f
Pavel Hofman <pavel.hofman@ivitera.com>
2019-11-17 10:00:06 +0100

    CTRL: catching communication exceptions in rcvInfo.m

--- octave/ctrl/rcvInfo.m
+++ octave/ctrl/rcvInfo.m
@@ -5,11 +5,16 @@ function info = rcvInfo(sock)
   persistent WAIT_MS = 50;
   info = [];
 
-  hasData = zmq_poll(sock, WAIT_MS);
-  if (hasData)
-    % info struct is long
-    % 0 = blocking mode
-    data = zmq_recv (sock, 50000, 0);
-    info = bytea2var(data);
-  endif
+  try
+    hasData = zmq_poll(sock, WAIT_MS);
+    if (hasData)
+      % info struct is long
+      % 0 = blocking mode
+      data = zmq_recv (sock, 50000, 0);
+      info = bytea2var(data);
+    endif
+  catch err
+    writeLog('ERROR', err.message);
+  end_try_catch
+
 endfunction
