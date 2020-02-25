f401a644998ddafde366dde8b70a324835d679f1
Pavel Hofman <pavel.hofman@ivitera.com>
2020-02-25 20:20:53 +0100

    UI: printStr.m accepts printf params

--- octave/ctrl/printStr.m
+++ octave/ctrl/printStr.m
@@ -1,9 +1,14 @@
-function printStr(str)
+function printStr(msg, varargin)
   global outBox;
   contents = get(outBox, 'string');
   if (rows(contents) == 0)
     contents = {};
   endif
-  contents(end + 1) = str;
+  if nargin() > 1
+    % formatting message
+    msg = sprintf(msg, varargin{:});
+  end
+
+  contents(end + 1) = msg;
   set(outBox, 'string', contents);
 endfunction
