44824fe344559627823ea27d866850478035f1f8
Pavel Hofman <pavel.hofman@ivitera.com>
2020-02-26 23:30:39 +0100

    UI: printing msg about stepper move in moveStepperTo.m

--- octave/ctrl/moveStepperTo.m
+++ octave/ctrl/moveStepperTo.m
@@ -2,6 +2,7 @@ function moveStepperTo(stepperID, steps)
   global ardStruct;
   
   writeLog('DEBUG', 'Moving stepper [%d] steps: %d', stepperID, steps);
+  printStr("Moving stepper [%d] steps: %d", stepperID, steps);
   ardStruct.ard.relMoveTo(stepperID, steps);
   % remembering
   global steppers;
