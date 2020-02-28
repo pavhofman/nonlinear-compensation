98ff422177602831fff9fc4764fa78ee2b549199
Pavel Hofman <pavel.hofman@ivitera.com>
2020-02-28 23:07:00 +0100

    UI: zoomCalLevels.m - minor variable renames, range extracted to constant

--- octave/ctrl/zoomCalLevels.m
+++ octave/ctrl/zoomCalLevels.m
@@ -1,14 +1,16 @@
 function zoomCalLevels(calFreqReq, targetLevels)
+  % TODO - should be function parameter
+  persistent DIFF_FROM_MID_DB = 0.1;
   global recStruct;
   
-  targetLevels = 20*log10(targetLevels);
+  targetLevelsDB = 20*log10(targetLevels);
   
   if ~isempty(calFreqReq)
     % must have at least 2 channels
     for channelID = 1:2
       plotStruct = recStruct.calPlots{channelID};
       calFreqReqCh = calFreqReq{channelID};
-      targetLevelsCh = targetLevels(channelID);      
+      targetLevelsCh = targetLevelsDB(channelID);
       % TODO - somehow show both frequencies
       % only first freq for now
       if ~isempty(calFreqReqCh) && any(~isna(calFreqReqCh(1, 2)))
@@ -18,19 +20,20 @@ function zoomCalLevels(calFreqReq, targetLevels)
         xVertices = [0.1 0.9 0.9 0.1];
         yVertices = [minAmpl minAmpl maxAmpl maxAmpl];
         rangePatch = plotStruct.rangePatch;
-        set(rangePatch, 'xdata', xVertices)
-        set(rangePatch, 'ydata', yVertices)
+        set(rangePatch, 'xdata', xVertices);
+        set(rangePatch, 'ydata', yVertices);
         setVisible(rangePatch, true);
         
         % zooming around target levels
-        lowerYLim = min(targetLevelsCh)  - 0.5;
-        upperYLim = min(targetLevelsCh)  + 0.5;
+        lowerYLim = min(targetLevelsCh)  - DIFF_FROM_MID_DB;
+        upperYLim = min(targetLevelsCh)  + DIFF_FROM_MID_DB;
         set(plotStruct.axis, 'ylim', [lowerYLim, upperYLim]);
       endif
 
       % updating target levels, if any
       if ~isna(targetLevelsCh)
         % update
+        % pos x = 0.5
         updateLevelsLine(targetLevelsCh, plotStruct.lastLine, 0.5);
       else
         % none, hide
