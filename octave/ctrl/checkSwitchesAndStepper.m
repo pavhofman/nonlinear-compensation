b78c8579a6df9d2b2623121df0c7b6377cda99df
Pavel Hofman <pavel.hofman@ivitera.com>
2020-03-07 14:42:07 +0100

    ADAPTER: checkSwitchesAndStepper() waits a bit between switches set and checking stepper, to allow propagation of level changes from the adapter to the measured signal. Without this delay the checkStepper.areLevelsStable check fired positive on new peaks which were however produced by previous switch configuration.

--- octave/ctrl/checkSwitchesAndStepper.m
+++ octave/ctrl/checkSwitchesAndStepper.m
@@ -3,13 +3,42 @@ function schedTask = checkSwitchesAndStepper(recInfo, playInfo, nextLabel, abort
   global adapterStruct;
   persistent checkingStepper = false;
 
-  % first checking continue status
+  % empty = not waiting for CHANGE_PROPAGATION_DELAY
+  persistent lastCheckTime = [];
+  % delay to wait before checking stepper after changed switches
+  persistent CHANGE_PROPAGATION_DELAY = 0.5;
+
+  % first checking whether switches are already set
   if adapterStruct.switchesSet
-    % CONTINUE button pressed, can continue with checking stepper
-    checkingStepper = true;
-    % setting flags
+    % switches set (CONTINUE button or relays set), can continue
+    % resetting flag
     adapterStruct.switchesSet = false;
-    adapterStruct.resetPrevMeasPeaks = true;
+    if adapterStruct.switchesChanged
+      % possible change in levels, requesting reset of historic peaks
+      adapterStruct.resetPrevMeasPeaks = true;
+      % waiting for CHANGE_PROPAGATION_DELAY
+      lastCheckTime = time();
+
+      % resetting flag
+      adapterStruct.switchesChanged = false;
+    else
+      % no switches change, directly to checking stepper
+      checkingStepper = true;
+    endif
+  endif
+
+  % checking for CHANGE_PROPAGATION_DELAY
+  if ~isempty(lastCheckTime)
+    if lastCheckTime + CHANGE_PROPAGATION_DELAY < time()
+      % waiting has expired
+      checkingStepper = true;
+      % resetting lastCheckTime
+      lastCheckTime = [];
+    else
+      % not yet
+      checkingStepper = false;
+      writeLog('TRACE', 'Waiting for switch change propagation');
+    endif
   endif
 
   if checkingStepper
