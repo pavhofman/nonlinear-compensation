11cfe33970c5469e03797d2947f99496e06e1546
Pavel Hofman <pavel.hofman@ivitera.com>
2020-05-15 18:25:05 +0200

    FILES: calling whole restart_chain.m in start_reading_file.m

--- octave/start_reading_file.m
+++ octave/start_reading_file.m
@@ -4,5 +4,5 @@ if sourceStruct.src == PLAYREC_SRC && ~structContains(sinkStruct, PLAYREC_SINK)
   clear playrec;
 endif
 sourceStruct.src = FILE_SRC;
+source 'restart_chain.m';
 source 'init_sourcename_status.m';
-restartReading = true;      
