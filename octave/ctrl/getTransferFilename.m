10fbb215348ac1a721ab46920507cabe5cc159b4
Pavel Hofman <pavel.hofman@ivitera.com>
2020-06-05 17:05:57 +0200

    TRANSFER: bugfix in getTransferFilename.m

--- octave/ctrl/getTransferFilename.m
+++ octave/ctrl/getTransferFilename.m
@@ -1,8 +1,8 @@
 % Detemines transfer filename for freq and circuit. Transfer files are stored in transfDir
-function filename = getTransferFilename(freq, extCircuit)
+function filepath = getTransferFilename(freq, extCircuit)
   global transfDir;
 
   % floating point freq is supported - rounding for now
-  filename = sprintf('transfer_%s_%s.dat', round(freq), extCircuit);
-  filename = getFilePath(filename, transfDir);
+  filename = sprintf('transfer_%d_%s.dat', round(freq), extCircuit);
+  filepath = getFilePath(filename, transfDir);
 endfunction
